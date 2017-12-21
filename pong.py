d1d7f3ae0c5003e2faba17260779865ea4a5e5a8
AP Cianflone <apcianflone@gmail.com>
2017-12-20 20:59:12 -0500

    init

--- pong.py
+++ pong.py
@@ -5,15 +5,12 @@ import gym
 
 # hyperparameters
 H = 200 # number of hidden layer neurons
-batch_size = 10 # every how many episodes to do a param update?
+batch_size = 10 # number of episodes before updating our network parameters
 learning_rate = 1e-4
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
 resume = False # resume from previous checkpoint?
 render = False
-
-# Optimization hyperparams
-norm = False
 
 # model initialization
 D = 80 * 80 # input dimensionality: 80x80 grid
@@ -24,8 +21,11 @@ else:
   model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
   model['W2'] = np.random.randn(H) / np.sqrt(H)
 
-grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } # update buffers that add up gradients over a batch
-rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } # rmsprop memory
+# There are many episodes before updating our parameters, these accumulate
+# in the gradient buffer
+grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() }
+# rmsprop memory
+rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() }
 
 def sigmoid(x):
   return 1.0 / (1.0 + np.exp(-x)) # sigmoid "squashing" function to interval [0,1]
@@ -44,37 +44,39 @@ def discount_rewards(r):
   discounted_r = np.zeros_like(r)
   running_add = 0
   for t in reversed(range(0, r.size)):
-    # Current discounted reward is current reward + future reward discounted
-    # If future has reward (r[t]), only take that to discount, otherwise
-    # recursively discount future reward if r[t] empty
-    if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
+    # running_add is the future reward to discount. If r[t] already has a
+    # reward, that means we are at the end of the game, there is no future
+    # state to discount, so running_add = 0
+    if r[t] != 0: running_add = 0
     running_add = running_add * gamma + r[t]
     discounted_r[t] = running_add
 
-  if norm:
-    discounted_r -= discounted_r.mean()
-    discounted_r /- discounted_r.std()
   return discounted_r
 
 def policy_forward(x):
   h = np.dot(model['W1'], x)
-  h[h<0] = 0 # ReLU nonlinearity
-  logp = np.dot(model['W2'], h)
+  h[h<0] = 0 # ReLU nonlinearity, wow!!
+  logp = np.dot(model['W2'], h) # logp is a scalar
   p = sigmoid(logp)
   return p, h # return probability of taking action 2, and hidden state
 
 def policy_backward(eph, epdlogp):
-  """ backward pass. (eph is array of intermediate hidden states) """
-  dW2 = np.dot(eph.T, epdlogp).ravel()
-  dh = np.outer(epdlogp, model['W2'])
-  dh[eph <= 0] = 0 # backpro prelu
-  dW1 = np.dot(dh.T, epx)
+  """
+  backward pass. (eph is array of intermediate hidden states)
+  Args:
+    eph: episode hidden states, stacked
+    epdlogp: episode action gradient with advantage
+  """
+  dW2 = np.dot(eph.T, epdlogp).ravel() # derivs wrt W2
+  dh = np.outer(epdlogp, model['W2']) # derivs wrt h post activation
+  dh[eph <= 0] = 0 # backprop relu
+  dW1 = np.dot(dh.T, epx) # derivs wrt W1
   return {'W1':dW1, 'W2':dW2}
 
 env = gym.make("Pong-v0")
-observation = env.reset()
+observation = env.reset() # start the game
 prev_x = None # used in computing the difference frame
-xs,hs,dlogps,drs = [],[],[],[]
+xs,hs,dlogps,drs = [],[],[],[] # x, h state, deriv predict, rewards
 running_reward = None
 reward_sum = 0
 episode_number = 0
@@ -94,7 +96,11 @@ while True:
   xs.append(x) # observation
   hs.append(h) # hidden state
   y = 1 if action == 2 else 0 # a "fake label"
-  dlogps.append(y - aprob) # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+  dlogps.append(y - aprob) # grad that encourages the action that was taken to
+  # be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+  # Later, we multiply this gradient by the reward. So if we take action 1, but
+  # lets say 'aprob' is quite, small, then the gradient is large. If reward for
+  # action 1 is positive, then we encourage our network to increase aprob
 
   # step the environment and get new measurements
   observation, reward, done, info = env.step(action)
@@ -102,30 +108,35 @@ while True:
 
   drs.append(reward) # record reward (has to be done after we call step() to get reward for previous action)
 
-  if done: # an episode finished
+  if done: # an episode finished, accumulate gradients or backprop
     episode_number += 1
 
     # stack together all inputs, hidden states, action gradients, and rewards for this episode
     epx = np.vstack(xs)
     eph = np.vstack(hs)
-    epdlogp = np.vstack(dlogps)
+    epdlogp = np.vstack(dlogps) # episode gradients of y - aprob
     epr = np.vstack(drs)
     xs,hs,dlogps,drs = [],[],[],[] # reset array memory
 
     # compute the discounted reward backwards through time
     discounted_epr = discount_rewards(epr)
-    # standardize the rewards to be unit normal (helps control the gradient estimator variance)
+    # standardize the rewards to be unit normal (helps control the grad var)
     discounted_epr -= np.mean(discounted_epr)
     discounted_epr /= np.std(discounted_epr)
 
-    epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
+    # Modulate the gradient with advantage (PG magic happens right here.)
+    epdlogp *= discounted_epr
     grad = policy_backward(eph, epdlogp)
+    # Gradients are accumulated over many episodes, determined by batch size
     for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
 
     # perform rmsprop parameter update every batch_size episodes
+    # RMSprop: divide the gradient by a running average of its recent magnitude
+    # see 6e, slide 26 of http://www.cs.toronto.edu/~tijmen/csc321/slides/lecture_slides_lec6.pdf
+    # for other opt, see: http://ruder.io/optimizing-gradient-descent/index.html#rmsprop
     if episode_number % batch_size == 0:
       for k,v in model.iteritems():
-        g = grad_buffer[k] # gradient
+        g = grad_buffer[k] # gradient for weight k
         rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
         model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
         grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
@@ -140,3 +151,6 @@ while True:
 
   if reward != 0: # Pong has either +1 or -1 reward exactly when game ends.
     print ('ep %d: game finished, reward: %f' % (episode_number, reward)) + ('' if reward == -1 else ' !!!!!!!!')
+
+
+
